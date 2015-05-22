2925bafc01c8c0355204436da267051512723f56
Peter Milne <helipilot50@gmail.com>
2015-05-22 06:54:18 -0600

    Updated POM to latest Aerospike client and Added license

--- src/main/java/com/aerospike/client/rest/JSONRecord.java
+++ src/main/java/com/aerospike/client/rest/JSONRecord.java
@@ -1,12 +1,24 @@
+/* 
+ * Copyright 2012-2015 Aerospike, Inc.
+ *
+ * Portions may be licensed to Aerospike, Inc. under one or more contributor
+ * license agreements.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.aerospike.client.rest;
 
-import java.util.Map;
-
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.aerospike.client.Record;
-import com.aerospike.client.query.ResultSet;
 
 /**
  * JSONRecord is used to convert an Aerospike Record
@@ -21,13 +33,6 @@ public class JSONRecord extends JSONObject {
 		put("generation", record.generation);
 		put("expiration", record.expiration);
 		put("bins", new JSONObject(record.bins));
-		if (record.duplicates != null){
-			JSONArray duplicates = new JSONArray();
-			for (Map<String, Object> duplicate : record.duplicates){
-				duplicates.add(new JSONObject(duplicate));
-			}
-			put("duplicates", duplicates);
-		}
 	}
 
 }
